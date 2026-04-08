855a19ba27213648b8148f72b5c1aad6136c124c
yutiantiankaixinqaq-a11y <yutiantiankaixinqaq@gmail.com>
2026-04-08 15:19:36 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -19,11 +19,20 @@ class TestCalculator(unittest.TestCase):
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
     #     fill in code
+    def test_multiply(self):
+        self.assertEqual(calculator.multiply(3, 4), 12)
+        self.assertEqual(calculator.multiply(-2, 5), -10)
+        self.assertEqual(calculator.multiply(0, 8), 0)
+
 
     # def test_divide(self): # 3 assertions
     #     fill in code
     # ##########################
-
+    def test_divide(self):
+        self.assertEqual(calculator.divide(2, 10), 5)
+        self.assertEqual(calculator.divide(5, -15), -3)
+        with self.assertRaises(ZeroDivisionError):
+            calculator.divide(0, 5)
     ######## Partner 2
     def test_divide_by_zero(self): # 1 assertion
     #     # call division function inside, example:
@@ -45,10 +54,22 @@ class TestCalculator(unittest.TestCase):
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
     #     #     logarithm(0, 5)
     #     fill in code
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            calculator.logarithm(-1, 10)
+        with self.assertRaises(ValueError):
+            calculator.logarithm(1, 10)
+        with self.assertRaises(ValueError):
+            calculator.logarithm(10, -5)
+
+
 
     # def test_hypotenuse(self): # 3 assertions
     #     fill in code
-
+    def test_hypotenuse(self):
+        self.assertAlmostEqual(calculator.hypotenuse(3, 4), 5.0)
+        self.assertAlmostEqual(calculator.hypotenuse(-3, 4), 5.0)
+        self.assertAlmostEqual(calculator.hypotenuse(5, 12), 13.0)
     # def test_sqrt(self): # 3 assertions
     #     # Test for invalid argument, example:
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
@@ -56,7 +77,11 @@ class TestCalculator(unittest.TestCase):
     #     # Test basic function
     #     fill in code
     ##########################
-
+    def test_sqrt(self):
+        self.assertAlmostEqual(calculator.square_root(16), 4.0)
+        self.assertAlmostEqual(calculator.square_root(0), 0.0)
+        with self.assertRaises(ValueError):
+            calculator.square_root(-9)
 # Do not touch this
 if __name__ == "__main__":
     unittest.main()
